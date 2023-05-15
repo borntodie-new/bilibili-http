6a91c53d7b379d29a72992e7a4b0ed6340e38838
jason <2631937454@qq.com>
2023-05-15 13:05:22 +0800

    测试静态路由

--- server_test.go
+++ server_test.go
@@ -11,10 +11,14 @@ func Login(ctx *Context) {
 func Register(ctx *Context) {
 	ctx.response.Write([]byte("register请求成功"))
 }
+func Index(ctx *Context) {
+	ctx.response.Write([]byte("首页"))
+}
 func TestHTTP_Start(t *testing.T) {
 	h := NewHTTP()
 	h.GET("/login", Login)
 	h.POST("/register", Register)
+	h.DELETE("/", Index)
 	err := h.Start(":8080")
 	if err != nil {
 		panic(err)
