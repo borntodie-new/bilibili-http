a7a9541cd25ae07fbe8b105257f2a057c8739461
jason <2631937454@qq.com>
2023-05-17 16:58:23 +0800

    测试匹配节点

--- router_test.go
+++ router_test.go
@@ -5,6 +5,7 @@ import (
 	"testing"
 )
 
+// TestRouterAdd 测试静态路由的注册节点功能
 func TestRouterAdd(t *testing.T) {
 	testCases := []struct {
 		name    string
@@ -51,6 +52,54 @@ func TestRouterAdd(t *testing.T) {
 		t.Run(tc.name, func(t *testing.T) {
 			r.addRouter(tc.method, tc.pattern, mockHandleFunc)
 			assert.PanicsWithError(t, tc.wantErr, func() {})
+		})
+	}
+}
+
+// TestRouterGet 测试静态路由的匹配节点功能
+func TestRouterGet(t *testing.T) {
+	testCases := []struct {
+		name     string
+		method   string
+		pattern  string
+		wantBool bool
+	}{
+		{
+			name:     "test1",
+			method:   "GET",
+			pattern:  "/study/login",
+			wantBool: true,
+		},
+		{
+			name:     "test2",
+			method:   "GET",
+			pattern:  "/study/login/action",
+			wantBool: true,
+		},
+		{
+			name:     "test3",
+			method:   "POST",
+			pattern:  "/study/login",
+			wantBool: true,
+		},
+		{
+			name:     "test2",
+			method:   "GET",
+			pattern:  "/study/login1",
+			wantBool: false,
+		},
+	}
+	r := newRouter()
+	var mockHandleFunc HandleFunc = func(ctx *Context) {}
+	r.addRouter("GET", "/study/login", mockHandleFunc)
+	r.addRouter("GET", "/study/login/action", mockHandleFunc)
+	r.addRouter("POST", "/study/login", mockHandleFunc)
+	r.addRouter("DELETE", "/study/login", mockHandleFunc)
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			_, ok := r.getRouter(tc.method, tc.pattern)
+			assert.Equal(t, tc.wantBool, ok)
+			// assert.Equal(t, mockHandleFunc, n.handleFunc)
 		})
 	}
 }
