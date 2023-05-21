25e251c828f3623c068546f6c9c68452ef3f724b
jason <2631937454@qq.com>
2023-05-21 21:56:34 +0800

    测试上下文功能

--- server_test.go
+++ server_test.go
@@ -1,19 +1,85 @@
 package bilibili_http
 
 import (
+	"fmt"
+	"net/http"
 	"testing"
 )
 
 func TestHTTP_Start(t *testing.T) {
 	h := NewHTTP()
 	h.GET("/study/login", func(ctx *Context) {
-		ctx.response.Write([]byte("静态路由 " + ctx.Pattern))
+		// ctx.response.Write([]byte("静态路由 " + ctx.Pattern))
+		ctx.TEXT(http.StatusOK, fmt.Sprintf("静态路由：%s", ctx.Pattern))
 	})
 	h.GET("/study/:course", func(ctx *Context) {
-		ctx.response.Write([]byte("参数路由 " + ctx.Pattern + "    " + ctx.params["course"]))
+		// ctx.response.Write([]byte("参数路由 " + ctx.Pattern + "    " + ctx.params["course"]))
+		course, err := ctx.Params("course")
+		if err != nil {
+			ctx.TEXT(http.StatusNotFound, "参数错误")
+			return
+		}
+		ctx.TEXT(http.StatusOK, fmt.Sprintf("参数路由：%s  %s", ctx.Pattern, course))
 	})
 	h.GET("/assets/*filepath", func(ctx *Context) {
-		ctx.response.Write([]byte("通融符路由" + ctx.Pattern + "    " + ctx.params["filepath"]))
+		// ctx.response.Write([]byte("通融符路由" + ctx.Pattern + "    " + ctx.params["filepath"]))
+		filepath, err := ctx.Params("filepath")
+		if err != nil {
+			ctx.TEXT(http.StatusNotFound, "参数错误")
+			return
+		}
+		ctx.TEXT(http.StatusOK, fmt.Sprintf("通融符路由：%s  %s", ctx.Pattern, filepath))
+	})
+
+	h.GET("/json", func(ctx *Context) {
+		ctx.JSON(http.StatusOK, H{
+			"code": 200,
+			"msg":  "请求成功",
+			"data": []string{
+				"A", "B", "C",
+			},
+		})
+	})
+	h.GET("/html", func(ctx *Context) {
+		ctx.HTML(http.StatusOK, `<h1 style="color:red;">hello world</h1>`)
+	})
+
+	h.GET("/query", func(ctx *Context) {
+		username, err := ctx.Query("username")
+		if err != nil {
+			ctx.SetStatusCode(http.StatusNotFound)
+			return
+		}
+		password, err := ctx.Query("password")
+		if err != nil {
+			ctx.SetStatusCode(http.StatusNotFound)
+			return
+		}
+		ctx.JSON(http.StatusOK, H{
+			"code":     200,
+			"msg":      "请求成功",
+			"username": username,
+			"password": password,
+		})
+	})
+
+	h.POST("/body", func(ctx *Context) {
+		type User struct {
+			Username string `json:"username"`
+			Password string `json:"password"`
+		}
+		var user User
+		err := ctx.BindJSON(&user)
+		if err != nil {
+			ctx.TEXT(http.StatusNotFound, err.Error())
+			return
+		}
+		ctx.JSON(http.StatusOK, H{
+			"code":     200,
+			"msg":      "请求成功",
+			"username": user.Username,
+			"password": user.Password,
+		})
 	})
 	err := h.Start(":8080")
 	if err != nil {
